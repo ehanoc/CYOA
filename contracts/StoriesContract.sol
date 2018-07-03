57e81107be72dd6ee4187630876554d2456ec636
ehanoc <bmartins.6018@gmail.com>
2018-07-03 13:57:04 +0100

    Add child node. Tests confirming adding a new child

--- contracts/StoriesContract.sol
+++ contracts/StoriesContract.sol
@@ -2,37 +2,66 @@ pragma solidity  ^0.4.19;
 
 contract StoriesContract {
 
-  event StoryCreated(string title, string body, uint id);
+  event OnNewStoryCreated(string title, string body, uint id);
+  event OnNewNodeCreated(string title, string body, uint parentId, uint id);
 
   struct StoryNode {
     uint id;
+
     string title;
     string body;
+
     address owner;
-    mapping(uint => StoryNode) childNodes;
+
+    // Id's of child nodes
+    //Lookups to be done in storiesNodes
+    uint[] childNodesIds;
   }
 
-  // story id maps to start node
-  mapping(uint => StoryNode) stories;
+  // All nodes
+  // Maps nodes Ids => Node Struct
+  // All nodes are held here for direct lookup vs looping through nested nodes
+  mapping(uint => StoryNode) storiesNodes;
 
-  // total nr of stories
+  // total nr of nodes
+  //also used to increment nodes Ids
   uint nr_nodes;
 
   /**
-  *
-  *
+  * Creates a new Story Root Node.
   *
   */
-  function createStory(string title, string body) public {
-    //require(msg.value > 0);
-
-    StoryNode memory newNode = StoryNode(nr_nodes, title, body, msg.sender);
-
-    stories[newNode.id] = newNode;
-
+  function createNewStoryNode(string title, string body) public {
+    StoryNode memory newNode = StoryNode(nr_nodes, title, body, msg.sender, new uint[](0));
+    storiesNodes[newNode.id] = newNode;
     nr_nodes++;
 
-    emit StoryCreated(title, body, nr_nodes);
+    emit OnNewStoryCreated(title, body, nr_nodes);
+  }
+
+  /**
+  * Adds a child node to the parent Node by id
+  *
+  * Requires parent node id to exist.
+  */
+  function addStoryNode(uint parentNodeId, string title, string body) public {
+    StoryNode storage parentNode = storiesNodes[parentNodeId];
+
+    StoryNode memory newNode = StoryNode(nr_nodes, title, body, msg.sender, new uint[](0));
+    storiesNodes[newNode.id] = newNode;
+    nr_nodes++;
+
+    parentNode.childNodesIds.push(newNode.id);
+
+    emit OnNewNodeCreated(title, body, parentNodeId, newNode.id);
+  }
+
+  /**
+  * Returns node's id, title, body, owner's address
+  */
+  function getStoryNode(uint id) public view returns(uint, string, string, address) {
+      StoryNode storage node = storiesNodes[id];
+      return (node.id, node.title, node.body, node.owner);
   }
 
   /**
--- test/story_contract_test.js
+++ test/story_contract_test.js
@@ -5,6 +5,8 @@ contract("StoriesContract", function(accounts) {
     //replace for your test contract addr
     //const contract = StoriesContract.at("0x2c2b9c9a4a25e24b174f26114e8926a9f2128fe4");
     const contract = StoriesContract.deployed();
+    const gas = 200000;
+    const account = accounts[0];
 
     describe("createStory", () => {
         it("it should let us create a story", () => {
@@ -12,9 +14,9 @@ contract("StoriesContract", function(accounts) {
                 var title = "test title";
                 var body = "test body";
 
-                return instance.createStory(title, body, { from: accounts[0], gas : 200000});
+                return instance.createNewStoryNode(title, body, {from: account, gas: gas});
             }).then(result => {
-                let storyCreatedEvent = getEventArgs(result, "StoryCreated");
+                let storyCreatedEvent = getEventArgs(result, "OnNewStoryCreated");
 
                 assert.equal(storyCreatedEvent.id > 0, true, "Should increment stories ID");
             }).catch(err => {
@@ -26,9 +28,32 @@ contract("StoriesContract", function(accounts) {
             return contract.then(instance => {
                 return instance.getNumberNodes.call();
             }).then(nrNodes => {
-                console.log(` nrNodes : ${nrNodes}`);
+                //console.log(` nrNodes : ${nrNodes}`);
                 assert.equal(nrNodes.valueOf() > 0, true, "No stories");
             })
+        });
+    });
+
+
+    describe("ChildNodes", () => {
+        var nodeCreatedEvent;
+
+        it("adding a new child node", () => {
+            return contract.then(instance => {
+                var parentId = 0; //assuming to test this exists
+                return instance.addStoryNode(parentId, "child node title", "child node body", {from: account, gas: gas});
+            }).then(result => {
+                nodeCreatedEvent = getEventArgs(result, "OnNewNodeCreated");
+                assert.equal(Number(nodeCreatedEvent.id) > Number(nodeCreatedEvent.parentId), true, "Has incremented node count");
+            });
+        });
+
+        it("confirm child added", () => {
+            return contract.then(instance => {
+                return instance.getStoryNode(nodeCreatedEvent.id);
+            }).then(node => {
+                assert.equal(Number(nodeCreatedEvent.id) == Number(node[0]), true);
+            });
         });
     });
 });
